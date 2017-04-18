cf7cd02edc5a20885b15fc87cb2474b393b9414b
pawel.zak <pawel.zak@lingaro.com>
2017-04-18 13:06:01 +0200

    beter bigger faster

--- grails-app/init/pzak/BootStrap.groovy
+++ grails-app/init/pzak/BootStrap.groovy
@@ -6,12 +6,16 @@ class BootStrap {
 
         def puggerfly = new Creature(
                 name: "Puggerfly",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non finibus sapien. Curabitur tincidunt pulvinar neque eu ultricies. Duis ac feugiat quam. Vestibulum est massa, scelerisque vitae purus ut, dapibus mollis nisi. Maecenas ut tortor sed ex luctus congue. Sed dui ante, mattis et maximus nec, efficitur nec nulla. Nulla faucibus nunc at velit lacinia euismod. Sed sed cursus ligula. Morbi tempus volutpat mi. Vivamus blandit, enim et ornare blandit, eros massa posuere tellus, ac vestibulum ligula mauris et ligula. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et.",
+                description: "According to legends only the one with pure hart and pockets full of treats can see the Puggerfly. \"Their song, though irresistibly sweet, was no less sad than sweet, and lapped both body and soul in a fatal lethargy.\" The ones that seen a puggerfly have lost ability to leave them. Their unrealistic beauty captivates one for lifetime.",
                 link:"https://pbs.twimg.com/media/B-z__bHUEAAB0o0.jpg:large"
         ).save()
         def puggerpillar = new Creature(name: "Puggerpillar",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non finibus sapien. Curabitur tincidunt pulvinar neque eu ultricies. Duis ac feugiat quam. Vestibulum est massa, scelerisque vitae purus ut, dapibus mollis nisi. Maecenas ut tortor sed ex luctus congue. Sed dui ante, mattis et maximus nec, efficitur nec nulla. Nulla faucibus nunc at velit lacinia euismod. Sed sed cursus ligula. Morbi tempus volutpat mi. Vivamus blandit, enim et ornare blandit, eros massa posuere tellus, ac vestibulum ligula mauris et ligula. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et.y"
-                , link:"https://i.ytimg.com/vi/6YLosC0x_wo/maxresdefault.jpg"
+                description: "The beauty of a Puggerpillar Is not in the clothes it wears, The figure that it carries, Or the way it combs its hair. The beauty of a Puggerpillar must be seen from in its eyes, Because that is the doorway to its heart, the place where love resides. The beauty of a Puggerpillar is not in a facial mole, But true beauty in a Puggerpillar Is reflected in its soul. It is the caring that it lovingly gives, The passion that it shows, And the beauty of a Puggerpillar With passing years only grows.",
+                link:"https://i.ytimg.com/vi/6YLosC0x_wo/maxresdefault.jpg"
+        ).save()
+        def pugmpkin = new Creature(name: "Pugmpkin",
+                description: "Each year, as the last of the crops come to harvest, there's a chance that you might see a Pumpkin Monster standing near your fields! Some scholars conjecture that they're the result of plant and earth magics coming together at this special time of the year. Others say they're just hungry and looking for a snack."
+                , link:"http://cdn.pcwallart.com/images/pug-in-pumpkin-costume-wallpaper-2.jpg"
         ).save()
 
 
